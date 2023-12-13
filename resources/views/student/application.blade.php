a35c1b83e482f25e627601810e227008142a4db8
AkmaralAkmaral <dzhanyshbekova@gmail.com>
2023-12-13 16:03:15 +0600

    Application2

--- resources/views/student/application.blade.php
+++ resources/views/student/application.blade.php
@@ -16,6 +16,7 @@
     </div>
 
 
+
    <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
@@ -26,89 +27,88 @@
                         {{csrf_field()}}
 
 
-
-                        <div class="form-group">
-                            <label for="fio">Full Name:</label>
-                            <input type="text" name="fio" required>
+                        <div class="mb-4">
+                            <label for="fio" class="block text-sm font-medium text-gray-700">Full Name:</label>
+                            <input type="text" name="fio" class="mt-1 p-2 border rounded-md w-full" required>
                         </div>
 
-                        <div class="form-group">
-                            <label for="birth_date">Birth Date:</label>
-                            <input type="date" name="birth_date" required>
+                        <div class="mb-4">
+                            <label for="birth_date" class="block text-sm font-medium text-gray-700">Birth Date:</label>
+                            <input type="date" name="birth_date" class="mt-1 p-2 border rounded-md w-full" required>
                         </div>
 
-                        <div class="form-group">
-                            <label for="nationality"> Nationality: </label>
-                            <input type="text" name="nationality" required>
+                        <div class="mb-4">
+                            <label for="nationality" class="block text-sm font-medium text-gray-700"> Nationality: </label>
+                            <input type="text" name="nationality" class="mt-1 p-2 border rounded-md w-full" required>
                         </div>
 
-                        <div class="form-group">
-                            <label for="gender">Gender:</label>
-                            <select name="gender" required>
+                       <div class="mb-4">
+                            <label for="gender" class="block text-sm font-medium text-gray-700">Gender:</label>
+                            <select name="gender" class="mt-1 p-2 border rounded-md w-full" required>
                                 <option value="male">Male</option>
                                 <option value="female">Female</option>
                             </select>
                         </div>
 
 
-                        <div class="form-group">
-                            <label for="passport_id">Passport ID:</label>
-                            <input type="text" name="passport_id" required>
+                        <div class="mb-4">
+                            <label for="passport_id"class="block text-sm font-medium text-gray-700">Passport ID:</label>
+                            <input type="text" name="passport_id" class="mt-1 p-2 border rounded-md w-full" required>
                         </div>
 
-                        <div class="form-group">
-                            <label for="issuing_authority">Issuing Authority:</label>
-                            <input type="text" name="issuing_authority" required>
+                        <div class="mb-4">
+                            <label for="issuing_authority" class="block text-sm font-medium text-gray-700">Issuing Authority:</label>
+                            <input type="text" name="issuing_authority" class="mt-1 p-2 border rounded-md w-full" required>
                         </div>
 
-                        <div class="form-group">
-                            <label for="inn">IIN (Individual Identification Number):</label>
-                            <input type="text" name="inn" required>
+                        <div class="mb-4">
+                            <label for="inn" class="block text-sm font-medium text-gray-700" >IIN (Individual Identification Number):</label>
+                            <input type="text" name="inn" class="mt-1 p-2 border rounded-md w-full" required>
                         </div>
 
-                         <div class="form-group">
-                            <label for="stat_ph_path_img">Statement Photo:</label>
+                        <div class="mb-4">
+                            <label for="stat_ph_path_img" class="block text-sm font-medium text-gray-700">Statement Photo:</label>
                             <input type="file" name="stat_ph_path_img" required>
-                            <input type="text" name="description_stat_ph" placeholder="Description for Statement Photo">
                         </div>
 
-                        <div class="form-group">
-                            <label for="edu_w_cert_path_img">Education Work Certificate Photo:</label>
+                       <div class="mb-4">
+                            <label for="edu_w_cert_path_img" class="block text-sm font-medium text-gray-700">Education Work Certificate Photo:</label>
                             <input type="file" name="edu_w_cert_path_img" required>
-                            <input type="text" name="description_edu_w_cert" placeholder="Description for Education Work Certificate Photo">
                         </div>
 
-                        <div class="form-group">
-                            <label for="ph_3_4_path_img">3x4 Photo:</label>
+                        <div class="mb-4">
+                            <label for="ph_3_4_path_img" class="block text-sm font-medium text-gray-700">3x4 Photo:</label>
                             <input type="file" name="ph_3_4_path_img" required>
-                            <input type="text" name="description_ph_3_4" placeholder="Description for 3x4 Photo">
                         </div>
 
-                        <div class="form-group">
-                            <label for="pay_rec_path_img">Payment Receipt Photo:</label>
+                        <div class="mb-4">
+                            <label for="pay_rec_path_img" class="block text-sm font-medium text-gray-700">Payment Receipt Photo:</label>
                             <input type="file" name="pay_rec_path_img" required>
-                            <input type="text" name="description_pay_rec" placeholder="Description for Payment Receipt Photo">
                         </div>
 
-                        <div class="form-group">
-                            <label for="med_cert_path_img">Medical Certificate Photo:</label>
+                        <div class="mb-4">
+                            <label for="med_cert_path_img" class="block text-sm font-medium text-gray-700">Medical Certificate Photo:</label>
                             <input type="file" name="med_cert_path_img" required>
-                            <input type="text" name="description_med_cert" placeholder="Description for Medical Certificate Photo">
                         </div>
 
-                        <div class="form-group">
-                            <label for="fluor_path_img">Fluorography Photo:</label>
+                        <div class="mb-4">
+                            <label for="fluor_path_img" class="block text-sm font-medium text-gray-700">Fluorography Photo:</label>
                             <input type="file" name="fluor_path_img" required>
-                            <input type="text" name="description_fluor" placeholder="Description for Fluorography Photo">
                         </div>
 
-                        <div class="form-group">
-                            <label for="residence_address">Residence Address:</label>
-                            <input type="text" name="residence_address" required>
+                        <div class="mb-4">
+                            <label for="residence_address" class="block text-sm font-medium text-gray-700">Residence Address:</label>
+                            <textarea type="text" name="residence_address" class="mt-1 p-2 border rounded-md w-full" required></textarea>
                         </div>
 
 
-                        <button class="btn btn-default" type="submit">Send Application</button>
+                        <div class="flex items-center gap-4">
+                            <x-primary-button>{{ __('Send Application') }}</x-primary-button>
+                        </div>
+
+
+
+
                     </form>
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,10 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+
+            backgroundColor: {
+                'red': '#ff0000',
+            },
         },
     },
 
